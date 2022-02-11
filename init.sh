c8631cc8ff2451c6938b25eb51970858fb674412
kami988 <kami988.eng@gmail.com>
2022-02-11 14:06:44 +0000

    mod: container 上のgit urlを強制

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ cd /
 git config --global user.name ${GITHUB_USER_NAME}
 git config --global user.email ${GITHUB_EMAIL}
 git config --global url."https://${GITHUB_USER_NAME}:${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
-
+git config --global url."https://${GITHUB_USER_NAME}:${GITHUB_TOKEN}@".insteadOf "git@"
 # if [ ! -d /repo/.git ]; then
 #   git clone https://github.com/kami988/go-1.18-sample.git repo
 # fi
