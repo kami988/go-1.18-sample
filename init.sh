98040f1fb5d5a73b82b13e26c76124418d5424cc
kami988 <kami988.eng@gmail.com>
2022-02-11 13:08:37 +0000

    mod: volume修正

--- init.sh
+++ init.sh
@@ -5,9 +5,9 @@ git config --global user.name ${GITHUB_USER_NAME}
 git config --global user.email ${GITHUB_EMAIL}
 git config --global url."https://${GITHUB_USER_NAME}:${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
 
-if [ ! -d /repo/.git ]; then
-  git clone https://github.com/kami988/go-1.18-sample.git repo
-fi
+# if [ ! -d /repo/.git ]; then
+#   git clone https://github.com/kami988/go-1.18-sample.git repo
+# fi
 
 cd /repo/
 sh -c "while :; do sleep 10; done"
